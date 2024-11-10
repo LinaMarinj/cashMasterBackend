afddcb8b29888fb6377bf1b6b9ae0335c85d44b1
Lina Marcela Marin <linamarin2011@gmail.com>
2024-11-10 16:27:53 -0500

    se crea carpeta del proyecto

---
+++ src/main/java/com/mycompany/cashmaster/CashMaster.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cashmaster;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class CashMaster {
+
+    public static void main(String[] args) {
+        
+    }
+}
